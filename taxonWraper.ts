a18a7ff73370901b21b597b0403525771fea80f2
Kushal Gautam <kushal.gtm385@gmail.com>
2021-07-19 15:33:15 -0400

    initial files

--- taxonWraper.ts
+++ taxonWraper.ts
@@ -17,15 +17,17 @@ commonName: "CommonName"
 
 const SPECIFY_API_ENDPOINT = "http://localhost:8080/api/specify"; // just for testing; 
 
-async function getTaxon(fullName: string): Promise<?Taxon> {
-    try{
-    const url: string =`${SPECIFY_API_ENDPOINT}/taxon?name=${fullName}`;
-    const result: ?Taxon = await fetch(url).then((res: Response) => res.json());
-    return result;
-  } catch (e){
-      console.log("An error occured"); 
-      return ; 
-  }
-}
 
- document.body.textContent = taxonWrapper(taxon);
+async function getTaxon(fullName: string): Promise<Taxon| null> {
+    try {
+      const url: string = `${SPECIFY_API_ENDPOINT}/taxon?name=${fullName}`;
+      return await fetch(url).then(res => res.json());
+    } catch (e) {
+      console.log("An error occured", e);
+      return null;
+    }
+  }
+  document.body.textContent=taxonWrapper(taxon);
+ console.log(JSON.stringify(getTaxon("diadema")));
+  console.log("Test"); 
+  
